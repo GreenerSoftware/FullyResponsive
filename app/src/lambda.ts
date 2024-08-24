f468c236deee3b28afa8b544542d2750a3a3a9b6
David Carboni <david@carboni.io>
2024-08-25 00:11:45 +0100

    Add ContextBuilder and ApplicationConfig - let's see if we can handle sessions.

--- app/src/lambda.ts
+++ app/src/lambda.ts
@@ -5,18 +5,41 @@ import {
   Context,
 } from 'aws-lambda';
 import { apiHandler } from '@scloud/lambda-api';
-import { Request, Response } from '@scloud/lambda-api/dist/types';
+import { ContextBuilder, Request, Response } from '@scloud/lambda-api/dist/types';
 import routes from './routes';
 import { slackLog } from './helpers/slack';
+import { sessionGet, sessionPut } from 'helpers/yar';
+import { ApplicationConfig } from 'application-config';
+import { env } from 'helpers/util';
+
+const config: ApplicationConfig = {
+  pathPrefix: '/deer-return',
+  // axios,
+  apiEndpoint: env('DEER_API_URL'),
+  hostPrefix: env('DEER_HOST_PREFIX'),
+  sessionSecret: env('DEER_SESSION_SECRET'),
+  gazetteerApiEndpoint: env('PC_LOOKUP_API_URL'),
+  feedbackUrl: env('DEER_FEEDBACK_URL'),
+  underTest: Boolean(env('UNDER_TEST'),
+};
 
 async function errorHandler(request: Request, e: Error): Promise<Response> {
   await slackLog(`${e.stack}`);
   return { statusCode: 500, body: { error: `Internal server error: ${request.path}` } };
 }
 
+const sessionHandler: ContextBuilder = async (request: Request) => {
+  request.context.sessionGet = <T>(key: string): T => {
+    return sessionGet(key, request);
+  };
+  request.context.sessionPut = <T>(key: string, value: T, response: Response) => {
+    sessionPut(key, value, request, response);
+  };
+};
+
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   try {
-    const result = await apiHandler(event, context, routes, errorHandler);
+    const result = await apiHandler(event, context, routes(config), errorHandler, undefined, sessionHandler);
     return result;
   } catch (e) {
     await slackLog(`${(e as Error).stack}`);
