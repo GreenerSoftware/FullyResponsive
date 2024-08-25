76c56f5fab3d40c9cacc15141207347d36744dc9
David Carboni <david@carboni.io>
2024-08-25 01:31:36 +0100

    Improve session handling so we don't overwrite and adding (improvised) flashing

--- app/src/helpers/yar.ts
+++ app/src/helpers/yar.ts
@@ -1,16 +1,45 @@
 import { Request, Response } from "@scloud/lambda-api/dist/types";
 
-export function sessionGet(key: string, request: Request) {
-  const data = request.cookies.yar || '{}';
+function getSession(request: Request): Record<string, any> {
+  if (request.context.session) return request.context.session;
+
+  let data = request.cookies.session || '{}';
   const session = JSON.parse(data);
+  request.context.session = session;
+  return session;
+}
+
+function setSession(session: Record<string, any>, request: Request, response: Response) {
+  request.context.session = session;
+
+  const data = JSON.stringify(session);
+  response.cookies = response.cookies || {};
+  response.cookies.session = data;
+}
+
+export function unFlash(request: Request) {
+  const session = getSession(request);
+  session['_flash'] = {};
+  // Can't currently do this from the lambda-api handler because there's no Response object available.
+  // setSession(session, request, response);
+}
+
+export function sessionGet(key: string, request: Request) {
+  const session = getSession(request);
   return session[key];
 }
 
 export function sessionSet(key: string, value: any, request: Request, response: Response) {
-  let data = request.cookies.yar || '{}';
-  const session = JSON.parse(data);
+  const session = getSession(request);
   session[key] = value;
-  data = JSON.stringify(session);
-  response.cookies = response.cookies || {};
-  response.cookies.yar = data;
+  setSession(session, request, response);
+}
+
+export function sessionFlash(request: Request, response: Response, type?: string, message?: any, isOverride?: boolean) {
+  const session = getSession(request);
+  session['_flash'] = session['_flash'] || {};
+  if (message) session['_flash'][type || 'default'] = message;
+  setSession(session, request, response);
+
+  return session['_flash'][type || 'default'];
 }
