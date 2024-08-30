c560998a311f6e3a30f98d766dfd5f067c6594e2
David Carboni <david@carboni.io>
2024-08-30 22:36:22 +0100

    Add additional page routes

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,10 +1,17 @@
 
 import { Routes } from '@scloud/lambda-api/dist/types';
 import { welcomePage } from 'pages/01-welcome/01-welcome.page';
+import whatIsYourEmailPage from 'pages/20-personal-details/20-personal-details.page';
+import checkYourAnswersAnnualPage from 'pages/100-check-your-answers-annual/100-check-your-answers-annual.page';
+import cullSubmittedAnnualPage from 'pages/40-submitted/40-submitted.page';
+
 
 
 export function routes(config: ApplicationConfig): Routes {
   const { path: welcomePagePath, scloudHandler: welcomePageHandler } = welcomePage(config);
+  const { path: whatIsYourEmailPagePath, scloudHandler: whatIsYourEmailPageHandler } = whatIsYourEmailPage(config);
+  const { path: checkYourAnswersAnnualPagePath, scloudHandler: checkYourAnswersAnnualPageHandler } = checkYourAnswersAnnualPage(config);
+  const { path: cullSubmittedAnnualPagePath, scloudHandler: cullSubmittedAnnualPageHandler } = cullSubmittedAnnualPage(config);
   return {
     '/': {
       GET: async () => ({
@@ -43,6 +50,9 @@ export function routes(config: ApplicationConfig): Routes {
     },
 
     [welcomePagePath]: { GET: welcomePageHandler },
+    [whatIsYourEmailPagePath]: { GET: whatIsYourEmailPageHandler },
+    [checkYourAnswersAnnualPagePath]: { GET: checkYourAnswersAnnualPageHandler },
+    [cullSubmittedAnnualPagePath]: { GET: cullSubmittedAnnualPageHandler },
   };
 };
 
