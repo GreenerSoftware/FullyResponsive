dd714f9b67b1a9c0147bdb369b05ab326859c9a3
David Carboni <david@carboni.io>
2024-09-06 16:10:16 +0100

    Add Slack logging

--- .infrastructure/lib/fullyresponsive-stack.ts
+++ .infrastructure/lib/fullyresponsive-stack.ts
@@ -2,10 +2,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {
-  BuildsBucket, WebApp, githubActions,
+  BuildsBucket, QueueFunction, WebApp, githubActions,
 } from '@scloud/cdk-patterns';
 import { Code } from 'aws-cdk-lib/aws-lambda';
 import { HostedZone, IHostedZone } from 'aws-cdk-lib/aws-route53';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 // import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 
 // Credentials
@@ -35,21 +37,24 @@ export default class FullyresponsiveStack extends cdk.Stack {
     // This only needs to be created once per account. If you already have one, you can delete this.
     githubActions(this).ghaOidcProvider();
 
-    // You'll need a zone to create DNS records in. This will need to be referenced by a real domain name so that SSL certificate creation can be authorised.
-    // NB the DOMAIN_NAME environment variable is defined in .infrastructure/secrets/domain.sh
-    const zone = this.zone(DOMAIN_NAME, ZONE_ID);
-
     // A bucket to hold zip files for Lambda functions
     // This is useful because updating a Lambda function in the infrastructure might set the Lambda code to a default placeholder.
     // Having a bucket to store the code in means we can update the Lambda function to use the code, either here in the infrastructure build, or from the Github Actions build.
     const builds = new BuildsBucket(this);
+
+    // You'll need a zone to create DNS records in. This will need to be referenced by a real domain name so that SSL certificate creation can be authorised.
+    // NB the DOMAIN_NAME environment variable is defined in .infrastructure/secrets/domain.sh
+    const zone = this.zone(DOMAIN_NAME, ZONE_ID);
+
+    // Slack
+    const slackQueue = this.slack(builds);
 
     // Create the frontend and API using Cloudfront
     // The following calls will create variables in Github Actions that can be used to deploy the frontend and API:
     // * API_LAMBDA - the name of the Lambda function to update when deploying the API
     // * CLOUDFRONT_BUCKET - for uploading the frontend
     // * CLOUDFRONT_DISTRIBUTIONID - for invalidating the Cloudfront cache
-    WebApp.node(this, 'app', zone, DOMAIN_NAME, false, false, {
+    const webApp = WebApp.node(this, 'app', zone, DOMAIN_NAME, false, false, {
       environment: {
         DEER_API_URL: `https://${DOMAIN_NAME}/deer-api/v1`,
         DEER_HOST_PREFIX: `https://${DOMAIN_NAME}`,
@@ -57,6 +62,7 @@ export default class FullyresponsiveStack extends cdk.Stack {
         // gazetteerApiEndpoint: process.env.PC_LOOKUP_API_URL ?? '',
         // feedbackUrl: process.env.DEER_FEEDBACK_URL ?? 'https://www.google.com',
         // underTest: Boolean(process.env.UNDER_TEST),
+        SLACK_QUEUE_URL: slackQueue.queueUrl,
       },
       functionProps: {
         memorySize: 3008,
@@ -64,6 +70,7 @@ export default class FullyresponsiveStack extends cdk.Stack {
       },
       handler: 'lambda.handler',
     });
+    slackQueue.grantSendMessages(webApp.lambda);
 
     // Set up OIDC access from Github Actions - this enables builds to deploy updates to the infrastructure
     githubActions(this).ghaOidcRole({ owner: env('OWNER'), repo: env('REPO') });
@@ -86,5 +93,20 @@ export default class FullyresponsiveStack extends cdk.Stack {
     return new HostedZone(this, 'zone', {
       zoneName,
     });
+  }
+
+  /**
+   * Component to send Slack messages for general logging.
+   */
+  slack(builds: Bucket): Queue {
+    const { queue } = QueueFunction.node(this, 'slack', {
+      environment: {
+        SLACK_WEBHOOK: process.env.SLACK_WEBHOOK || '',
+      },
+      functionProps: {
+        code: Code.fromBucket(builds, 'slack.zip'),
+      },
+    });
+    return queue;
   }
 }
